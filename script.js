5ece92438d9ebe3f95a1ce0c456c41ef2cd0c085
N8Dawgg <natwheeler1337@gmail.com>
2023-08-25 13:28:25 -0400

    Added the 'calcBtnClicked' function and get it to check button id.

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 const inputField = document.querySelector('#input-field');
+const calcBtnList = document.querySelectorAll('.calc-btn');
+
+function start() {
+    calcBtnList.forEach((e) => {
+        e.addEventListener('click',calcBtnClicked);
+    });
+}
+
+start();
+
+function calcBtnClicked(e) {
+    console.log(e.target.id);
+}
